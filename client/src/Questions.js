fc50c2ecb8bdec24dbdb51be45f6244a6c825c39
Katarzyna Zielińska <eaakjz@students.eaaa.dk>
2020-04-20 19:43:10 +0200

    Change "answers" word based on number of words

--- client/src/Questions.js
+++ client/src/Questions.js
@@ -4,11 +4,20 @@ import { Link } from "@reach/router";
 class Questions extends Component {
   render() {
     let list = [];
+
     this.props.data.forEach((elm) => {
+      let numberOfAnswers = elm.answers.length;
+      let grammar;
+      if (numberOfAnswers === 1) {
+        grammar = "answer";
+      } else {
+        grammar = "answers";
+      }
+
       list.push(
         <li key={elm._id} className="question_card">
           <Link to={"/question/" + elm._id}> {elm.text} </Link>
-          <span>{elm.answers.length} answers</span>
+          <span>{numberOfAnswers + " " + grammar}</span>
         </li>
       );
     });
